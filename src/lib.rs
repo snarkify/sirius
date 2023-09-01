fe662f743ea649f6df6ac1c8252f64f285f20a34
cyphersnake <cyphersnake@pm.me>
2023-09-01 12:36:12 +0400

    mv `plonk::Error` to `mod error`

--- src/lib.rs
+++ src/lib.rs
@@ -12,20 +12,4 @@ pub mod poseidon;
 pub mod table;
 pub mod util;
 
-/// Wrapper for [`halo2_proofs::plonk::Error`] to
-/// impl [`PartialEq`] & [`Eq`] and be able to use
-/// it in [`assert_eq`] and other comparisons
-#[derive(Debug)]
-pub struct Halo2PlonkError(halo2_proofs::plonk::Error);
-
-impl PartialEq for Halo2PlonkError {
-    fn eq(&self, _other: &Self) -> bool {
-        matches!(self, _other)
-    }
-}
-impl Eq for Halo2PlonkError {}
-impl From<halo2_proofs::plonk::Error> for Halo2PlonkError {
-    fn from(value: halo2_proofs::plonk::Error) -> Self {
-        Self(value)
-    }
-}
+pub mod error;
