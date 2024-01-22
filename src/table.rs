bec5e75938504b10ab18edb519fdbb7a952c7d7f
Chao Ma <mc8305@gmail.com>
2024-01-22 12:42:40 +0700

    add module description for table.rs

--- src/table.rs
+++ src/table.rs
@@ -1,3 +1,19 @@
+//! This module implements the core functionalities to (1) obtain all necessary information from halo2
+//! circuits and (2) run special soundness protocols.
+//! It centers around the `TableData` struct, which encapsulates the PLONK constraint system and
+//! handles the construction and operation of various PLONK components. Key features include:
+//!
+//! - Preparation and assembly of the constraint system
+//! - Implementation of special soundness protocols (`run_sps_protocol_*` functions), essential for
+//!   generating instance/witnesses/challenges securely
+//! - Construction of permutation matrices, ensuring copy constraints consistency in the constraint system.
+//! - Construction of lookup Arguments when the circuits contains lookup argument
+//! - Comprehensive error handling through the `SpsError` enum, addressing potential issues in
+//!   constraint system setup and execution.
+//!
+//! The module is the intermediate data representation of plonkish constrain system defined by the
+//! circuits
+
 use crate::{
     commitment::CommitmentKey,
     concat_vec,
