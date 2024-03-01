3de5c5298a6b73b8a2b8426f7f33e1dd6640cc53
cyphersnake <57308842+cyphersnake@users.noreply.github.com>
2024-03-01 10:15:49 +0100

    feat(sfc): impl `halo2::Circuit` for `StepFoldingCircuit` (#181)
    **Motivation**
    Previously we tried to avoid double synth of step_circuit to count
    `z_output` and what to do folding, however, because of X0,X1 we still do
    double synth (conventionally off-circuit & on-circuit), so now we can
    generalize `StepFoldingCircuit` as `halo2::Circuit` and use a single
    API-design for the `ConstraintSystem` based on `halo2::Circuit`
    
    Taked from #170 & refactored
    
    **Overview**
    - rm copy of halo2 `FloorPlanner`
    - rm `type FloorPlanner` from step circuit
    - use `process_step` result for `z_next` in IVC instead of
    `synthetize_step`

--- src/ivc/mod.rs
+++ src/ivc/mod.rs
@@ -2,11 +2,11 @@ pub mod step_circuit;
 
 pub mod step_folding_circuit;
 
-mod floor_planner;
 mod fold_relaxed_plonk_instance_chip;
 mod incrementally_verifiable_computation;
 mod instance_computation;
 mod public_params;
 
+pub use halo2_proofs::circuit::SimpleFloorPlanner;
 pub use incrementally_verifiable_computation::*;
 pub use public_params::{CircuitPublicParamsInput, PublicParams};
