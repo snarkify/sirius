470e8654044d9375f9e81859b2aafb6b8c856787
cyphersnake <57308842+cyphersnake@users.noreply.github.com>
2024-04-01 14:44:05 +0200

    feat(poly): impl `GroupedPoly::new` (#219)
    **Motivation**
    Creation of `GroupedPoly`
    Part of #159
    
    **Overview**
    - The implementation follows the idea of `fold_transform`.
    - Multiplication by `F` is implemented
    - Implemented a simple test of `GroupedPoly` creation
    - The plan is to add a benchmark for this function based on a real
    `StepFoldingCircuit` expression

--- src/polynomial/grouped_poly.rs
+++ src/polynomial/grouped_poly.rs
@@ -1,7 +1,9 @@
-use std::ops::{Add, Mul, Sub};
+use std::ops::{Add, Mul, Neg, Sub};
 
 use ff::PrimeField;
 use itertools::*;
+
+use crate::polynomial::Query;
 
 use super::Expression;
 
@@ -40,6 +42,59 @@ impl<IT: IntoIterator<Item = (usize, Expression<F>)>, F: PrimeField> From<IT> fo
 }
 
 impl<F: PrimeField> GroupedPoly<F> {
+    /// This function converts an arbitrary expression into a [`GroupedPoly`], by selecting new
+    /// [`Expression::Polynomial`] & [`Expression::Challenge`]
+    ///
+    /// Since they are labelled using a sequential index, to select new ones we need to know the
+    /// ones that are not already occupied. Moreover, we need a one-to-one correspondence for each
+    /// polynomial and challenge.
+    ///
+    /// Therefore, `num_of_poly` & `num_of_challenge` are used as input parameters, which allow us to
+    /// allocate a polynomial `x + num_of_poly` for polynomial `x` and for challenge respectively.
+    ///
+    /// ## Example
+    ///
+    /// ```math
+    /// P(a, b, c, d, e) &= (a + b + c) * (d + e) =>
+    ///     [a1 + b1 + c1 + k * (a2 + b2 + c2)] & [d1 + e1 + k * (d2 + e2)]
+    ///     =
+    ///     (a1 + b1 + c1)(d1 + e1)                             * k^0 +
+    ///     [(a2 + b2 + c2)(d1 + e1) + (a1 + b1 + c1)(d2 + e2)] * k^1 +
+    ///     (a2 + b2 + c2)(d2 + e2)                             * k^2
+    ///     =
+    ///     a1*d1 + a1*e1 + b1*d1 + b1*e1 + c1*d1 + c1*e1       * k^0 +
+    ///     a2*d1 + a2*e1 + b2*d1 + b2*e1 + c2*d1 + c2*e1 + a1*d2 + a1*e2 + b1*d2 + b1*e2 + c1*d2 + c1*e2 * k^1 +
+    ///     a2*d2 + a2*e2 + b2*d2 + b2*e2 + c2*d2 + c2*e2       * k^2
+    /// ```
+    fn new(expr: Expression<F>, num_of_poly: usize, num_of_challenge: usize) -> Self {
+        expr.evaluate(
+            &|constant| GroupedPoly {
+                terms: vec![Some(Expression::Constant(constant))],
+            },
+            &|poly| {
+                let y = Expression::Polynomial(Query {
+                    index: poly.index + num_of_poly,
+                    rotation: poly.rotation,
+                });
+
+                GroupedPoly {
+                    terms: vec![Some(Expression::Polynomial(poly)), Some(y)],
+                }
+            },
+            &|challenge_index| {
+                let y = Expression::Challenge(challenge_index + num_of_challenge);
+
+                GroupedPoly {
+                    terms: vec![Some(Expression::Challenge(challenge_index)), Some(y)],
+                }
+            },
+            &|a| -a,
+            &|a, b| a + b,
+            &|a, b| a * b,
+            &|a, k| a * k,
+        )
+    }
+
     fn iter(&self) -> impl Iterator<Item = (usize, &Expression<F>)> {
         self.terms
             .iter()
@@ -79,6 +134,24 @@ macro_rules! impl_poly_ops {
 
 impl_poly_ops!(Add, add, Sum, std::convert::identity);
 impl_poly_ops!(Sub, sub, Sum, std::ops::Neg::neg);
+
+impl<F: PrimeField> Mul<F> for GroupedPoly<F> {
+    type Output = Self;
+
+    fn mul(self, rhs: F) -> Self::Output {
+        Self {
+            terms: self
+                .terms
+                .into_iter()
+                .map(|expr| {
+                    expr.map(|expr| {
+                        Expression::Product(Box::new(Expression::Constant(rhs)), Box::new(expr))
+                    })
+                })
+                .collect(),
+        }
+    }
+}
 
 impl<F: PrimeField> Mul for GroupedPoly<F> {
     type Output = GroupedPoly<F>;
@@ -121,9 +194,23 @@ impl<F: PrimeField> Mul for GroupedPoly<F> {
     }
 }
 
+impl<F: PrimeField> Neg for GroupedPoly<F> {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        Self {
+            terms: self
+                .terms
+                .into_iter()
+                .map(|expr| expr.map(Neg::neg))
+                .collect(),
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
-    use crate::polynomial::Query;
+    use std::array;
 
     use ff::Field;
     use halo2_proofs::poly::Rotation;
@@ -134,7 +221,7 @@ mod test {
 
     #[test]
     fn simple_add() {
-        let mut actual = GroupedPoly::<Fq>::from(map! {
+        let actual = GroupedPoly::<Fq>::from(map! {
             0 => Expression::Constant(Fq::from_u128(u128::MAX)),
             1 => Expression::Polynomial(Query {
                 index: 0,
@@ -153,7 +240,6 @@ mod test {
         .iter()
         .map(|(degree, term)| format!("{degree};{}", term.expand()))
         .collect::<Vec<_>>();
-        actual.sort();
 
         assert_eq!(
             actual,
@@ -168,7 +254,7 @@ mod test {
 
     #[test]
     fn simple_sub() {
-        let mut actual = GroupedPoly::<Fq>::from(map! {
+        let actual = GroupedPoly::<Fq>::from(map! {
             0 => Expression::Constant(Fq::from_u128(u128::MAX)),
             1 => Expression::Polynomial(Query {
                 index: 0,
@@ -187,7 +273,6 @@ mod test {
         .iter()
         .map(|(degree, term)| format!("{degree};{}", term.expand()))
         .collect::<Vec<_>>();
-        actual.sort();
 
         assert_eq!(
             actual,
@@ -202,7 +287,7 @@ mod test {
 
     #[test]
     fn simple_mul() {
-        let mut actual = GroupedPoly::<Fq>::from(map! {
+        let actual = GroupedPoly::<Fq>::from(map! {
             9 => Expression::Sum(
                 Box::new(Expression::Polynomial(Query { index: 0, rotation: Rotation(0) })),
                 Box::new(Expression::Polynomial(Query { index: 1, rotation: Rotation(1) }))
@@ -217,14 +302,13 @@ mod test {
         .iter()
         .map(|(degree, term)| format!("{degree};{}", term.expand()))
         .collect::<Vec<_>>();
-        actual.sort();
 
         assert_eq!(actual, vec!["18;(Z_0)(Z_2)(Z_3) + (Z_2)(Z_3)(Z_1[+1])"]);
     }
 
     #[test]
     fn mul() {
-        let mut actual = GroupedPoly::<Fq>::from(map! {
+        let actual = GroupedPoly::<Fq>::from(map! {
                 2 => Expression::Polynomial(Query { index: 0, rotation: Rotation(0) }),
                 3 => Expression::Polynomial(Query { index: 1, rotation: Rotation(0) }),
                 4 => Expression::Polynomial(Query { index: 2, rotation: Rotation(0) }),
@@ -237,7 +321,6 @@ mod test {
         .iter()
         .map(|(degree, term)| format!("{degree};{}", term.expand()))
         .collect::<Vec<_>>();
-        actual.sort();
 
         assert_eq!(
             actual,
@@ -247,6 +330,55 @@ mod test {
                 "6;(Z_2)(Z_3) + (Z_1)(Z_4) + (Z_0)(Z_5)",
                 "7;(Z_2)(Z_4) + (Z_1)(Z_5)",
                 "8;(Z_2)(Z_5)"
+            ]
+        );
+    }
+
+    #[test]
+    fn creation() {
+        // P(a, b, c, d, e) &= (a + b + c) * (d + e) =>
+        //     [a1 + b1 + c1 + k* (a2 + b2 + c2)] & [d1 + e1 + k * (d2 + e2)]
+        //
+        //     =
+        //
+        //     (a1 + b1 + c1)(d1 + e1)                             * k^0 +
+        //     [(a2 + b2 + c2)(d1 + e1) + (a1 + b1 + c1)(d2 + e2)] * k^1 +
+        //     (a2 + b2 + c2)(d2 + e2)                             * k^2
+        //
+        //     =
+        //
+        //     a1*d1 + a1*e1 + b1*d1 + b1*e1 + c1*d1 + c1*e1       * k^0 +
+        //     a2*d1 + a2*e1 + b2*d1 + b2*e1 + c2*d1 + c2*e1 + a1*d2 + a1*e2 + b1*d2 + b1*e2 + c1*d2 + c1*e2 * k^1 +
+        //     a2*d2 + a2*e2 + b2*d2 + b2*e2 + c2*d2 + c2*e2       * k^2
+
+        fn sum(expr: &[Expression<Fq>]) -> Box<Expression<Fq>> {
+            Box::new(match expr.split_first() {
+                Some((first, rest)) => Expression::Sum(Box::new(first.clone()), sum(rest)),
+                None => Expression::Constant(Fq::ZERO),
+            })
+        }
+
+        let [a, b, c, d, e] = array::from_fn(|index| {
+            Expression::Polynomial(Query {
+                index,
+                rotation: Rotation(0),
+            })
+        });
+
+        let grouped_poly =
+            GroupedPoly::new(Expression::Product(sum(&[a, b, c]), sum(&[d, e])), 5, 0);
+
+        let actual = grouped_poly
+            .iter()
+            .map(|(degree, term)| format!("{degree};{}", term.expand()))
+            .collect::<Vec<_>>();
+
+        assert_eq!(
+            actual,
+            vec![
+                "0;(Z_0)(Z_3) + (Z_1)(Z_3) + (Z_2)(Z_3) + (Z_0)(Z_4) + (Z_1)(Z_4) + (Z_2)(Z_4)",
+                "1;(Z_3)(Z_5) + (Z_4)(Z_5) + (Z_3)(Z_6) + (Z_4)(Z_6) + (Z_3)(Z_7) + (Z_4)(Z_7) + (Z_0)(Z_8) + (Z_1)(Z_8) + (Z_2)(Z_8) + (Z_0)(Z_9) + (Z_1)(Z_9) + (Z_2)(Z_9)",
+                "2;(Z_5)(Z_8) + (Z_6)(Z_8) + (Z_7)(Z_8) + (Z_5)(Z_9) + (Z_6)(Z_9) + (Z_7)(Z_9)"
             ]
         );
     }
