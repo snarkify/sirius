05b9bce3e7cbd31a037a6558c8a6864fe04e0d43
cyphersnake <cyphersnake@pm.me>
2024-05-22 18:30:42 +0200

    refactor(poly): group modules

--- src/polynomial/mod.rs
+++ src/polynomial/mod.rs
@@ -1,9 +1,9 @@
 pub mod expression;
 pub mod graph_evaluator;
 pub mod grouped_poly;
+pub mod lagrange;
 pub mod sparse;
 pub mod univariate;
 
 pub use expression::{ColumnIndex, Expression, Query, QueryType};
-
-pub mod lagrange;
+pub use lagrange::iter_eval_lagrange_polynomials_for_cyclic_group;
