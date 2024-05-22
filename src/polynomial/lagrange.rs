1acfa5d9d4c67668a3c6098332ed8a8ca722a940
cyphersnake <cyphersnake@pm.me>
2024-05-22 16:18:08 +0200

    feat(poly): migrate from symbolic eval

--- src/polynomial/lagrange.rs
+++ src/polynomial/lagrange.rs
@@ -1,27 +1,28 @@
-use std::iter;
+use std::{iter, num::NonZeroUsize};
 
 use ff::PrimeField;
 
-use super::{expression::challenge_in_degree, Expression};
+use crate::fft;
 
-pub struct Fraction<F> {
-    numerator: Expression<F>,
-    divider: Expression<F>,
-}
+/// Lazy eval the values of the Lagrange polynomial for a cyclic subgroup of length `n` at
+/// the `challenge` point
+///
+/// Panic, if challenge will be equal to any element of cyclic subgroup, because division by zero
+pub fn iter_eval_lagrange_polynomials_for_cyclic_group<F: PrimeField>(
+    challenge: F,
+    n: NonZeroUsize,
+) -> impl Iterator<Item = F> {
+    let n = n.get();
 
-pub fn get_lagrange_polynomials_for_cyclic_group<F: PrimeField>(
-    challenge_index: usize,
-    k: usize,
-) -> impl Iterator<Item = Fraction<F>> {
-    let X = Expression::Challenge(challenge_index);
-    let X_pow_k = challenge_in_degree(challenge_index, k);
-    let n = Expression::Constant(F::from_u128((k + 1) as u128));
+    let generator: F = fft::get_omega_or_inv(n.ilog2(), false);
+    let cyclic_subgroup = iter::successors(Some(F::ONE), move |val| Some(*val * generator));
 
-    iter::successors(Some(Self::ONE), |el| Some(*el * F::ROOT_OF_UNITY))
-        .map(|value| Expression::Constant(value))
-        .map(move |value| Fraction {
-            numerator: value.clone() * (X_pow_k.clone() - Expression::Constant(F::ONE)),
-            divider: n.clone() * (X.clone() - value),
+    let inverted_n = F::from(n as u64).invert().unwrap();
+    let X_pow_n_sub_1 = challenge.pow([n as u64]) - F::ONE;
+
+    cyclic_subgroup
+        .map(move |value| {
+            (value * inverted_n) * (X_pow_n_sub_1 * (challenge - value).invert().unwrap())
         })
-        .take(k)
+        .take(n)
 }
