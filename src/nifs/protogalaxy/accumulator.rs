d1f095f6e625a27c2dde6f0a46072fcc7ced2c60
cyphersnake <cyphersnake@pm.me>
2024-07-29 21:40:44 +0200

    feat(nifs): impl `protogalaxy::verify`

--- src/nifs/protogalaxy/accumulator.rs
+++ src/nifs/protogalaxy/accumulator.rs
@@ -3,7 +3,7 @@ use std::iter;
 use crate::{
     ff::Field,
     halo2curves::CurveAffine,
-    plonk::{RelaxedPlonkTrace, RelaxedPlonkTraceArgs},
+    plonk::{RelaxedPlonkInstance, RelaxedPlonkTrace, RelaxedPlonkTraceArgs},
     poseidon::{AbsorbInRO, ROTrait},
     util,
 };
@@ -38,5 +38,37 @@ impl<C: CurveAffine> Accumulator<C> {
             e: C::ScalarExt::ZERO,
             trace: RelaxedPlonkTrace::new(args),
         }
+    }
+}
+
+pub struct AccumulatorInstance<C: CurveAffine> {
+    /// TODO#266 Docs
+    pub(super) betas: Box<[C::ScalarExt]>,
+    /// TODO#266 Docs
+    pub(super) U: RelaxedPlonkInstance<C>,
+    /// TODO#266 Docs
+    pub(super) e: C::ScalarExt,
+}
+
+impl<C: CurveAffine> From<Accumulator<C>> for AccumulatorInstance<C> {
+    fn from(value: Accumulator<C>) -> Self {
+        let Accumulator {
+            betas,
+            trace: RelaxedPlonkTrace { U, W: _ },
+            e,
+        } = value;
+
+        AccumulatorInstance { betas, U, e }
+    }
+}
+
+impl<C: CurveAffine, RO: ROTrait<C::Base>> AbsorbInRO<C::Base, RO> for AccumulatorInstance<C> {
+    fn absorb_into(&self, ro: &mut RO) {
+        ro.absorb(&self.U).absorb_field_iter(
+            self.betas
+                .iter()
+                .chain(iter::once(&self.e))
+                .map(|b| util::fe_to_fe::<C::ScalarExt, C::Base>(b).unwrap()),
+        );
     }
 }
