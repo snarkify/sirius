a79806e832ab7dbcdf05f07f432d7295bda54c1b
cyphersnake <57308842+cyphersnake@users.noreply.github.com>
2024-09-05 08:50:58 +0200

    feat(nifs): impl `protogalaxy::is_sat_acc`  (#339)
    **Motivation**
    Close #267
                                               
    **Overview**
    - impl `ProtoGalaxy::is_sat_acc`
    - impl `ProtoGalaxy::is_sat_perm`
    - finish tests for protogalaxy circuits

--- src/nifs/protogalaxy/tests.rs
+++ src/nifs/protogalaxy/tests.rs
@@ -1,18 +1,15 @@
 use halo2_proofs::{
+    dev::MockProver,
     halo2curves::{
         ff::{FromUniformBytes, PrimeFieldBits},
         group::prime::PrimeCurveAffine,
     },
     plonk::Circuit,
 };
+use tracing::info_span;
+use tracing_test::traced_test;
 
 use super::*;
-
-const T: usize = 3;
-const RATE: usize = 2;
-const R_F: usize = 4;
-const R_P: usize = 3;
-
 use crate::{
     commitment,
     halo2curves::bn256::G1Affine as Affine,
@@ -25,13 +22,17 @@ use crate::{
     table::{CircuitRunner, Witness},
 };
 
+const T: usize = 3;
+const RATE: usize = 2;
+const R_F: usize = 4;
+const R_P: usize = 3;
+const L: usize = 3;
+
 type Scalar = <Affine as CurveAffine>::ScalarExt;
 type Base = <Affine as CurveAffine>::Base;
 
 type RO<F> = PoseidonHash<F, T, RATE>;
 type Instance<F> = Vec<F>;
-
-const L: usize = 3;
 
 type ProtoGalaxy = crate::nifs::protogalaxy::ProtoGalaxy<Affine, L>;
 type ProverParam = <ProtoGalaxy as FoldingScheme<Affine, L>>::ProverParam;
@@ -57,11 +58,6 @@ struct Mock<CIRCUIT: Circuit<Scalar>> {
     S: PlonkStructure<Scalar>,
     ck: CommitmentKey<Affine>,
 
-    ro_nark_generate: RO<Base>,
-    ro_nark_verifier: RO<Base>,
-    ro_acc_prover: RO<Base>,
-    ro_acc_verifier: RO<Base>,
-
     circuits_ctx: [CircuitCtx; L],
 
     pp: ProverParam,
@@ -70,10 +66,25 @@ struct Mock<CIRCUIT: Circuit<Scalar>> {
     _p: PhantomData<CIRCUIT>,
 }
 
+fn ro<F: PrimeFieldBits + FromUniformBytes<64>>() -> PoseidonHash<F, T, RATE> {
+    PoseidonHash::<F, T, RATE>::new(Spec::<F, T, RATE>::new(R_F, R_P))
+}
+
 impl<C: Circuit<Scalar>> Mock<C> {
     pub fn new(k_table_size: u32, circuits: [(C, Vec<Scalar>); L]) -> Self {
-        let circuits_runners =
-            circuits.map(|(circuit, instance)| CircuitRunner::new(k_table_size, circuit, instance));
+        let circuits_runners = circuits.map(|(circuit, instance)| {
+            let instances = if instance.is_empty() {
+                vec![]
+            } else {
+                vec![instance.clone()]
+            };
+            MockProver::run(k_table_size, &circuit, instances)
+                .unwrap()
+                .verify()
+                .unwrap();
+
+            CircuitRunner::new(k_table_size, circuit, instance)
+        });
 
         let ck = commitment::setup_smallest_key(k_table_size, &circuits_runners[0].cs, b"");
         let S = circuits_runners[0]
@@ -82,16 +93,8 @@ impl<C: Circuit<Scalar>> Mock<C> {
 
         let (pp, vp) = ProtoGalaxy::setup_params(Affine::identity(), S.clone()).unwrap();
 
-        fn ro<F: PrimeFieldBits + FromUniformBytes<64>>() -> PoseidonHash<F, T, RATE> {
-            PoseidonHash::<F, T, RATE>::new(Spec::<F, T, RATE>::new(R_F, R_P))
-        }
-
         Mock {
             ck,
-            ro_nark_generate: ro(),
-            ro_nark_verifier: ro(),
-            ro_acc_prover: ro(),
-            ro_acc_verifier: ro(),
             circuits_ctx: circuits_runners.map(CircuitCtx::collect),
             pp,
             vp,
@@ -101,6 +104,8 @@ impl<C: Circuit<Scalar>> Mock<C> {
     }
 
     pub fn generate_plonk_traces(&mut self) -> [PlonkTrace<Affine>; L] {
+        let mut generate_ro = ro();
+        let mut is_sat_ro = ro();
         self.circuits_ctx
             .iter()
             .map(|ctx| {
@@ -109,9 +114,14 @@ impl<C: Circuit<Scalar>> Mock<C> {
                     &ctx.instance,
                     &ctx.witness,
                     &self.pp,
-                    &mut self.ro_nark_generate,
+                    &mut generate_ro,
                 )
                 .unwrap()
+            })
+            .inspect(|trace| {
+                self.S
+                    .is_sat(&self.ck, &mut is_sat_ro, &trace.u, &trace.w)
+                    .unwrap()
             })
             .collect::<Vec<_>>()
             .try_into()
@@ -121,28 +131,46 @@ impl<C: Circuit<Scalar>> Mock<C> {
     pub fn run(mut self) {
         let incoming = self.generate_plonk_traces();
 
-        let acc = ProtoGalaxy::new_accumulator(
-            AccumulatorArgs::from(&self.S),
+        let init_accumulator =
+            ProtoGalaxy::new_accumulator(AccumulatorArgs::from(&self.S), &self.pp, &mut ro());
+
+        ProtoGalaxy::is_sat_accumulation(&self.S, &init_accumulator)
+            .expect("The newly created accumulator is not satisfactory");
+
+        let (accumulator_from_prove, proof) = ProtoGalaxy::prove(
+            &self.ck,
             &self.pp,
-            &mut self.ro_acc_prover,
-        );
+            &mut ro(),
+            init_accumulator.clone(),
+            &incoming,
+        )
+        .expect("`protogalaxy::prove` failed");
 
-        let (accumulator, proof) =
-            ProtoGalaxy::prove(&self.ck, &self.pp, &mut self.ro_acc_prover, acc, &incoming)
-                .expect("`protogalaxy::prove` failed");
+        ProtoGalaxy::is_sat(&self.ck, &self.S, &accumulator_from_prove)
+            .expect("The accumulator after calling `prove` is not satisfactory");
 
-        let _acc_instance = ProtoGalaxy::verify(
+        let accumulator_from_verify = ProtoGalaxy::verify(
             &self.vp,
-            &mut self.ro_nark_verifier,
-            &mut self.ro_acc_verifier,
-            &AccumulatorInstance::from(accumulator),
+            &mut ro(),
+            &mut ro(),
+            &init_accumulator.into(),
             &incoming.map(|tr| tr.u),
             &proof,
         )
         .unwrap();
+
+        let accumulator_inst_from_prove = AccumulatorInstance::from(accumulator_from_prove);
+
+        assert_eq!(
+            accumulator_inst_from_prove.betas,
+            accumulator_from_verify.betas,
+        );
+        assert_eq!(accumulator_inst_from_prove.e, accumulator_from_verify.e,);
+        assert_eq!(accumulator_inst_from_prove.ins, accumulator_from_verify.ins);
     }
 }
 
+#[traced_test]
 #[test]
 fn random_linear_combination() {
     Mock::new(
@@ -174,14 +202,17 @@ fn random_linear_combination() {
     .run();
 }
 
+#[traced_test]
 #[test]
 fn fibo() {
+    let _s = info_span!("fibo").entered();
+
     const K: u32 = 4;
     const SIZE: usize = 16;
 
     let seq1 = get_fibo_seq(1, 1, SIZE);
     let seq2 = get_fibo_seq(2, 3, SIZE);
-    let seq3 = get_fibo_seq(4, 5, SIZE);
+    let seq3 = get_fibo_seq(3, 5, SIZE);
 
     Mock::new(
         10,
@@ -200,7 +231,7 @@ fn fibo() {
                     b: Scalar::from(seq2[1]),
                     num: SIZE,
                 },
-                vec![Scalar::from(seq1[SIZE - 1])],
+                vec![Scalar::from(seq2[SIZE - 1])],
             ),
             (
                 FiboCircuit {
@@ -208,15 +239,18 @@ fn fibo() {
                     b: Scalar::from(seq3[1]),
                     num: SIZE,
                 },
-                vec![Scalar::from(seq2[SIZE - 1])],
+                vec![Scalar::from(seq3[SIZE - 1])],
             ),
         ],
     )
     .run();
 }
 
+#[traced_test]
 #[test]
 fn fibo_lookup() {
+    let _s = info_span!("fibo_lookup").entered();
+
     const K: u32 = 5;
     const SIZE: usize = 7;
 
