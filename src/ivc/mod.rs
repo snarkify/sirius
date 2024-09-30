791eb2cbb4b419bf2296722ecd3133df32a28477
cyphersnake <57308842+cyphersnake@users.noreply.github.com>
2024-09-30 13:00:23 +0200

    feat(ivc): impl `instances_accumulator_computation` (#353)
    **Motivation**
    As part of #316, we need to accumulate `instance`, using a poseidon
    hash. This PR should provide a consistent way to do this
    {on,off}-circuit within nifs::vanilla
    
    **Overview**
    - New Module
    - Added `instances_accumulator_computation.rs` to handle the
    accumulation of `instances` using the Poseidon hash function.
    - Functions:
    - `fold_step_circuit_instances_hash_accumulator` for off-circuit hash
    accumulation.
    - `fold_assign_step_circuit_instances_hash_accumulator` for on-circuit
    hash accumulation
    - Testing:
    - Added comprehensive unit tests to verify the consistency of the
    accumulator computation.

--- src/ivc/mod.rs
+++ src/ivc/mod.rs
@@ -5,6 +5,7 @@ pub mod step_folding_circuit;
 mod consistency_markers_computation;
 mod fold_relaxed_plonk_instance_chip;
 mod incrementally_verifiable_computation;
+pub mod instances_accumulator_computation;
 mod public_params;
 
 pub use halo2_proofs::circuit::SimpleFloorPlanner;
