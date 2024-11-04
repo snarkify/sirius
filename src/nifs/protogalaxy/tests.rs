d4aeffdf8efac724dd9ccf3a80bb9791aceaf18f
cyphersnake <cyphersnake@pm.me>
2024-11-04 14:34:49 +0100

    feat(ivc): impl protogalaxy::fold_instances
    **Motivation**
    Last part of #361
    
    **Overview**
    Unlike the rest of the functionality, this one is untested because the
    test would need a cyclic curve and that makes the tests very difficult.
    
    It is better to test this part of e2e already in its entirety

--- src/nifs/protogalaxy/tests.rs
+++ src/nifs/protogalaxy/tests.rs
@@ -127,15 +127,19 @@ impl<C: Circuit<Scalar>> Mock<C> {
             .try_into()
             .unwrap()
     }
+    pub fn new_accumulator(&self) -> Accumulator {
+        let acc = ProtoGalaxy::new_accumulator(AccumulatorArgs::from(&self.S), &self.pp, &mut ro());
+
+        ProtoGalaxy::is_sat_accumulation(&self.S, &acc)
+            .expect("The newly created accumulator is not satisfactory");
+
+        acc
+    }
 
     pub fn run(mut self) {
         let incoming = self.generate_plonk_traces();
 
-        let init_accumulator =
-            ProtoGalaxy::new_accumulator(AccumulatorArgs::from(&self.S), &self.pp, &mut ro());
-
-        ProtoGalaxy::is_sat_accumulation(&self.S, &init_accumulator)
-            .expect("The newly created accumulator is not satisfactory");
+        let init_accumulator = self.new_accumulator();
 
         let (accumulator_from_prove, proof) = ProtoGalaxy::prove(
             &self.ck,
