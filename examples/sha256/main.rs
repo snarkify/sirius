5f4f508579ce7a9fb7e209645cfe2c8b9618c3a4
cyphersnake <cyphersnake@pm.me>
2025-02-17 15:35:35 +0100

    feat(examples): sha256 -> cyclefold
    **Motivation**
    There is no point of sangria::IVC for this circuit, due to the number of its gates
    
    **Overview**
    N/A

--- examples/sha256/main.rs
+++ examples/sha256/main.rs
@@ -1,6 +1,6 @@
 #![allow(dead_code)]
 
-use std::{array, io, iter, marker::PhantomData, num::NonZeroUsize, path::Path};
+use std::{array, env, io, iter, marker::PhantomData, path::Path};
 
 use bn256::G1 as C1;
 use grumpkin::G1 as C2;
@@ -8,16 +8,13 @@ use halo2_proofs::{
     circuit::{AssignedCell, Layouter, Value},
     plonk::ConstraintSystem,
 };
+use metadata::LevelFilter;
 use sirius::{
     commitment::CommitmentKey,
     ff::PrimeField,
     group::{prime::PrimeCurve, Group},
-    halo2curves::{bn256, grumpkin, CurveAffine, CurveExt},
-    ivc::{
-        sangria::{CircuitPublicParamsInput, PublicParams, IVC},
-        step_circuit, StepCircuit, SynthesisError,
-    },
-    poseidon::{self, ROPair},
+    halo2curves::{bn256, grumpkin, CurveAffine},
+    ivc::{cyclefold, StepCircuit, SynthesisError},
 };
 use tracing::*;
 
@@ -258,6 +255,7 @@ pub mod sha256 {
 }
 
 pub use sha256::{BlockWord, Sha256, Table16Chip, Table16Config};
+use tracing_subscriber::{fmt::format::FmtSpan, EnvFilter};
 
 #[derive(Default, Debug)]
 struct TestSha256Circuit<F: PrimeField> {
@@ -267,9 +265,9 @@ struct TestSha256Circuit<F: PrimeField> {
 // TODO
 const ARITY: usize = BLOCK_SIZE / 2;
 
-const CIRCUIT_TABLE_SIZE1: usize = 22;
-const CIRCUIT_TABLE_SIZE2: usize = 22;
-const COMMITMENT_KEY_SIZE: usize = 27;
+const PRIMARY_CIRCUIT_TABLE_SIZE: usize = 21;
+const COMMITMENT_KEY_SIZE: usize = 26;
+const FOLD_STEP_COUNT: usize = 5;
 
 impl<F: PrimeField> StepCircuit<ARITY, F> for TestSha256Circuit<F> {
     type Config = Table16Config;
@@ -324,16 +322,6 @@ impl<F: PrimeField> StepCircuit<ARITY, F> for TestSha256Circuit<F> {
     }
 }
 
-const T: usize = 5;
-const RATE: usize = 4;
-
-type RandomOracle = poseidon::PoseidonRO<T, RATE>;
-
-type RandomOracleConstant<F> = <RandomOracle as ROPair<F>>::Args;
-
-const LIMB_WIDTH: NonZeroUsize = unsafe { NonZeroUsize::new_unchecked(32) };
-const LIMBS_COUNT: NonZeroUsize = unsafe { NonZeroUsize::new_unchecked(10) };
-
 type C1Affine = <C1 as PrimeCurve>::Affine;
 type C2Affine = <C2 as PrimeCurve>::Affine;
 
@@ -351,64 +339,56 @@ fn get_or_create_commitment_key<C: CurveAffine>(
 }
 
 fn main() {
-    tracing_subscriber::fmt::init();
+    let builder = tracing_subscriber::fmt()
+        // Adds events to track the entry and exit of the span, which are used to build
+        // time-profiling
+        .with_span_events(FmtSpan::ENTER | FmtSpan::CLOSE)
+        // Changes the default level to INFO
+        .with_env_filter(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::INFO.into())
+                .from_env_lossy(),
+        );
 
-    info!("Start");
-    // C1
-    let sc1 = TestSha256Circuit::default();
-    // C2
-    let sc2 = step_circuit::trivial::Circuit::<ARITY, _>::default();
+    // Structured logs are needed for time-profiling, while for simple run regular logs are
+    // more convenient.
+    //
+    // So this expr keeps track of the --json argument for turn-on json-logs
+    if env::args().any(|arg| arg.eq("--json")) {
+        builder.json().init();
+    } else {
+        builder.init();
+    }
 
-    let primary_spec = RandomOracleConstant::<<C2 as CurveExt>::Base>::new(10, 10);
-    let secondary_spec = RandomOracleConstant::<<C1 as CurveExt>::Base>::new(10, 10);
+    // To osterize the total execution time of the example
+    let _span = info_span!("sha256_example").entered();
 
-    info!("Start generate");
-    let primary_commitment_key = get_or_create_commitment_key(COMMITMENT_KEY_SIZE, "grumpkin")
-        .expect("Failed to get primary key");
-    info!("Primary generated");
-    let secondary_commitment_key = get_or_create_commitment_key(COMMITMENT_KEY_SIZE, "bn256")
-        .expect("Failed to get secondary key");
-    info!("Secondary generated");
+    let primary = TestSha256Circuit::default();
 
-    let pp = PublicParams::<
-        '_,
-        ARITY,
-        ARITY,
-        T,
-        C1Affine,
-        C2Affine,
-        TestSha256Circuit<_>,
-        step_circuit::trivial::Circuit<ARITY, _>,
-        RandomOracle,
-        RandomOracle,
-    >::new(
-        CircuitPublicParamsInput::new(
-            CIRCUIT_TABLE_SIZE1 as u32,
-            &primary_commitment_key,
-            primary_spec,
-            &sc1,
-        ),
-        CircuitPublicParamsInput::new(
-            CIRCUIT_TABLE_SIZE2 as u32,
-            &secondary_commitment_key,
-            secondary_spec,
-            &sc2,
-        ),
-        LIMB_WIDTH,
-        LIMBS_COUNT,
-    )
-    .unwrap();
-    info!("public params: {pp:?}");
+    let primary_commitment_key =
+        get_or_create_commitment_key::<C1Affine>(COMMITMENT_KEY_SIZE, "bn256")
+            .expect("Failed to get primary key");
+    let secondary_commitment_key =
+        get_or_create_commitment_key::<C2Affine>(COMMITMENT_KEY_SIZE, "grumpkin")
+            .expect("Failed to get secondary key");
 
-    IVC::fold_with_debug_mode(
-        &pp,
-        &sc1,
-        array::from_fn(|i| C1Scalar::from_u128(i as u128)),
-        &sc2,
-        array::from_fn(|i| C2Scalar::from_u128(i as u128)),
-        NonZeroUsize::new(1).unwrap(),
+    let mut pp = cyclefold::PublicParams::new(
+        &primary,
+        primary_commitment_key,
+        secondary_commitment_key,
+        PRIMARY_CIRCUIT_TABLE_SIZE as u32,
     )
     .unwrap();
 
-    debug!("base case ready");
+    let primary_input = array::from_fn(|i| C1Scalar::from(i as u64));
+
+    let mut ivc = cyclefold::IVC::new(&mut pp, &primary, primary_input).expect("while step=0");
+
+    for step in 1..=FOLD_STEP_COUNT {
+        ivc = ivc
+            .next(&pp, &primary)
+            .unwrap_or_else(|err| panic!("while step={step}: {err:?}"));
+    }
+
+    ivc.verify(&pp).expect("while verify");
 }
